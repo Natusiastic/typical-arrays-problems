7a8b79524896f326e0748189879fa519c261f2ab
natusiastic <natusiastic@gmail.com>
2020-10-01 17:09:29 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array)) {
+    if (array.length > 0) {
+      let arr = array;
+      let result = arr[0];
+      for (let i = 1; i < arr.length; i++) {
+        if (result > arr[i]) result = arr[i];
+      }
+      return result;
+    } else return 0;
+  } else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array)) {
+    if (array.length > 0) {
+      let arr = array;
+      let result = arr[0];
+      for (let i = 1; i < arr.length; i++) {
+        if (result < arr[i]) result = arr[i];
+      }
+      return result;
+    } else return 0;
+  } else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array)) {
+    if (array.length > 0) {
+      let arr = array; 
+      let summ = 0;
+      for (let i = 0; i < arr.length; i++) {
+        summ += arr[i];
+      }
+      return summ / (arr.length);
+    } else return 0;
+  } else return 0;
 }
